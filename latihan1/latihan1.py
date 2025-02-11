cc13bed6f922545412af5f8ecfa3904fdce582a9
MFahrezii <trezi069@gmail.com>
2025-02-11 07:44:34 +0700

    program baru

---
+++ latihan1/latihan1.py
@@ -0,0 +1,10 @@
+def CetakIdentitas(Nama,Kelas,Jenis_Kelamin) :
+    print("Nama : ",Nama)
+    print("Kelas : ",Kelas)
+    print("Jenis Kelamin : ",Jenis_Kelamin)
+    
+# panggil fungsi cetak identitas siswa
+CetakIdentitas("Fida","X-PPLG","Laki-Laki")
+CetakIdentitas("Apen","X-PPLG","Laki-Laki")
+CetakIdentitas("Aziz","X-PPLG","Laki-Laki")
+CetakIdentitas("Rais","X-PPLG","Laki-Laki")
